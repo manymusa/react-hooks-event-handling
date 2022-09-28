6cb0f9d3f35a976863c08f7c86ae8af65d6affdf
manymusa <adanalin113@gmail.com>
2022-09-27 21:21:55 -0400

    commit tree e1727668ded78f4426011552afa01530c63667bf

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Tickler() {
-  function tickle() {
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
+
+//   return <button>Tickle me!</button>;
+// }
+
+function Tickler(){
+  function tickle (){
     console.log("Teehee!");
   }
-
-  return <button>Tickle me!</button>;
+  
+  return <button onClick={tickle}>Tickle me!</button>
 }
 
 export default Tickler;
